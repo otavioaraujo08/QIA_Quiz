955dcf40a8c836592449870a5a31a8b5fe249af2
Marcelo Chaves <Marcelochaves20000@gmail.com>
2022-04-21 10:57:27 -0300

    Home screen created

---
+++ qia_quiz/src/components/Logo/Logo.vue
@@ -0,0 +1,12 @@
+<template>
+  <img src="../../assets/logo.png" alt="logo" />
+</template>
+
+<script>
+
+import "./Styles.css"
+
+export default {
+  name: "MyLogo"
+}
+</script>
---
+++ qia_quiz/src/views/Home/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <logo />
+    <button type="button" class="btn btn-lg btn-success">Entrar</button>
+  </div>
+</template>
+
+<script>
+
+import Logo from "../../components/Logo/Logo.vue"
+import "./Styles.css"
+
+export default {
+  components: {
+    Logo
+  },
+  name: 'HomeView',
+}
+</script>
