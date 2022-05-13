61af2d021cd61658fbc698c895eab8588181106e
Otávio Araujo <51797012+otavioaraujo08@users.noreply.github.com>
2022-05-13 18:54:11 -0300

    Atualizações BD e Score de Matérias

--- qia_quiz/src/views/Score/ScorePortugues.vue
+++ qia_quiz/src/views/Score/ScorePortugues.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="score">
     <div>
-      <h3 v-if="this.items.pontuacao_recebida === 0">
-        Infelizmente você ficou {{ this.items.pontuacao_recebida }} pontos :C
+      <h3 v-if="pontuacao === 0">
+        Infelizmente você ficou {{ pontuacao }} pontos :C
       </h3>
       <h3 v-else>
-        Parabéns você ficou com <strong>{{ this.items.pontuacao_recebida }}</strong> pontos !!
+        Parabéns você ficou com <strong>{{ pontuacao }}</strong> pontos !!
       </h3>
     </div>
     <div>
@@ -18,51 +18,12 @@
 
 <script>
 import "./styles.css";
-import axios from "axios";
-import { useRoute } from 'vue-router'
-
 export default {
-  name: "ManageQuizView",
+  name: "ScorePortugues",
 
-   data(){
-    const routes = useRoute()
-
-    return{
-      item: [],
-      items: {
-        professor: "",
-        disciplina: "",
-        titulo: "",
-        comando: "",
-        resposta: "",
-        pergunta_n1: "",
-        pergunta_n2: "",
-        pergunta_n3: "",
-        pontuacao: "",
-        pontuacao_recebida: "",
-        peso: ""
-      },
-      routes,
-      escolha: ""
-    }
+  props: {
+    pontuacao: Number
   },
-
-  mounted(){
-    this.receberQuiz(this.routes.params.id)
-  },
-
-  methods: {
-    // Receber os valores referentes aos quizes
-    async receberQuiz(id) {
-        try {
-          const data = await axios.patch(`${`http://localhost:3000/portugues`}/${id}`,{});
-          this.items = data.data
-          console.log(this.items)
-        }catch (error) {
-          console.error(error);
-        }
-    }
-  }
 };
 </script>
 
