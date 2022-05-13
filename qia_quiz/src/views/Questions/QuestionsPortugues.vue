6c4b9cad48fc201e3f8369ec8c492ea91462964b
Otávio Araujo <51797012+otavioaraujo08@users.noreply.github.com>
2022-05-13 18:54:29 -0300

    Função Quiz

--- qia_quiz/src/views/Questions/QuestionsPortugues.vue
+++ qia_quiz/src/views/Questions/QuestionsPortugues.vue
@@ -1,73 +1,218 @@
 <template>
   <div class="student">
-    <div class="form-floating">
-      <textarea class="form-control" disabled id="floatingTextarea2" style="height: 100px"></textarea>
-      <label for="floatingTextarea">{{this.items.comando}}</label>
+    <div v-if="finalizado == false">
+      <div v-for="(item, index) of items" :key="item.id">
+        <div v-if="indice == index && indice != items.length - 1 && indice == 0">
+          <div class="form-floating">
+            <textarea class="form-control" disabled id="floatingTextarea2" style="height: 100px"></textarea>
+            <label for="floatingTextarea">{{item.comando}}</label>
+          </div>
+          <div id="botoes" class="d-flex flex-column">
+            <div class="d-flex align-items-center justify-content-around mt-3">
+              <div id="resposta">
+                <button 
+                  type="button" 
+                  class="btn-lg btn-primary"
+                  @click="()=> {pontuacao = parseInt(item.pontuacao); id = 1; compararResposta(id, pontuacao);}"
+                >
+                  {{item.resposta}}
+                </button> 
+              </div>
+              <div id="pergunta_um">
+                <button 
+                  type="button" 
+                  class="btn-lg btn-success"
+                  @click="()=> {pontuacao = parseInt(item.pontuacao); id = 2; status= false; compararResposta(id, pontuacao);}"
+                >
+                  {{item.pergunta_n1}}
+                </button>
+              </div>
+            </div>
+            <div class="d-flex align-items-center justify-content-around mt-3">
+              <div id="pergunta_dois">
+                <button 
+                  type="button" 
+                  class="btn-lg btn-danger"
+                  @click="()=> {pontuacao = parseInt(item.pontuacao); id = 3; status= false; compararResposta(id, pontuacao);}"
+                >
+                  {{item.pergunta_n2}}
+                </button>
+              </div>
+              <div id="pergunta_tres">
+                <button 
+                  type="button" 
+                  class="btn-lg btn-warning"
+                  @click="()=> {pontuacao = parseInt(item.pontuacao); id = 4; status= false; compararResposta(id, pontuacao);}"
+                  >
+                   {{item.pergunta_n3}}
+                </button>
+              </div>
+            </div>
+            <div>
+              <button 
+                type="button" 
+                class="btn-lg btn-primary" 
+                @click="()=> {indice += 1; status = false}"
+              >
+                Próxima Questão
+              </button>
+            </div>
+          </div>
+        </div>
+        <div v-if="indice == index && indice != items.length - 1 && indice != 0">
+          <div class="form-floating">
+            <textarea class="form-control" disabled id="floatingTextarea2" style="height: 100px"></textarea>
+            <label for="floatingTextarea">{{item.comando}}</label>
+          </div>
+          <div id="botoes" class="d-flex flex-column">
+            <div class="d-flex align-items-center justify-content-around mt-3">
+              <div id="resposta">
+                <button 
+                  type="button" 
+                  class="btn-lg btn-primary"
+                  @click="()=> {pontuacao = parseInt(item.pontuacao); id = 1; compararResposta(id, pontuacao);}"
+                >
+                  {{item.resposta}}
+                </button> 
+              </div>
+              <div id="pergunta_um">
+                <button 
+                  type="button" 
+                  class="btn-lg btn-success"
+                  @click="()=> {pontuacao = parseInt(item.pontuacao); id = 2; status= false; compararResposta(id, pontuacao);}"
+                >
+                  {{item.pergunta_n1}}
+                </button>
+              </div>
+            </div>
+            <div class="d-flex align-items-center justify-content-around mt-3">
+              <div id="pergunta_dois">
+                <button 
+                  type="button" 
+                  class="btn-lg btn-danger"
+                  @click="()=> {pontuacao = parseInt(item.pontuacao); id = 3; status= false; compararResposta(id, pontuacao);}"
+                >
+                  {{item.pergunta_n2}}
+                </button>
+              </div>
+              <div id="pergunta_tres">
+                <button 
+                  type="button" 
+                  class="btn-lg btn-warning"
+                  @click="()=> {pontuacao = parseInt(item.pontuacao); id = 4; status= false; compararResposta(id, pontuacao);}"
+                  >
+                   {{item.pergunta_n3}}
+                </button>
+              </div>
+            </div>
+            <div>
+              <button 
+                type="button" 
+                class="btn-lg btn-primary" 
+                @click="()=> {indice -= 1; status = false}"
+              >
+                Questão Anterior
+              </button>
+              <button 
+                type="button" 
+                class="btn-lg btn-primary" 
+                @click="()=> {indice += 1; status = false}"
+              >
+                Próxima Questão
+              </button>
+            </div>
+          </div>
+        </div>
+        <div v-else-if="index == items.length -1 && indice == items.length - 1">
+          <div class="form-floating">
+            <textarea class="form-control" disabled id="floatingTextarea2" style="height: 100px"></textarea>
+            <label for="floatingTextarea">{{item.comando}}</label>
+          </div>
+          <div id="botoes" class="d-flex flex-column">
+            <div class="d-flex align-items-center justify-content-around mt-3">
+              <div id="resposta">
+                <button 
+                  type="button" 
+                  class="btn-lg btn-primary"
+                  @click="()=> {pontuacao = parseInt(item.pontuacao); id = 1; compararResposta(id, pontuacao);}"
+                >
+                  {{item.resposta}}
+                </button> 
+              </div>
+              <div id="pergunta_um">
+                <button 
+                  type="button" 
+                  class="btn-lg btn-success"
+                  @click="()=> {pontuacao = parseInt(item.pontuacao); id = 2; status= false; compararResposta(id, pontuacao);}"
+                >
+                  {{item.pergunta_n1}}
+                </button>
+              </div>
+            </div>
+            <div class="d-flex align-items-center justify-content-around mt-3">
+              <div id="pergunta_dois">
+                <button 
+                  type="button" 
+                  class="btn-lg btn-danger"
+                  @click="()=> {pontuacao = parseInt(item.pontuacao); id = 3; status= false; compararResposta(id, pontuacao);}"
+                >
+                  {{item.pergunta_n2}}
+                </button>
+              </div>
+              <div id="pergunta_tres">
+                <button 
+                  type="button" 
+                  class="btn-lg btn-warning"
+                  @click="()=> {pontuacao = parseInt(item.pontuacao); id = 4; status= false; compararResposta(id, pontuacao);}"
+                  >
+                   {{item.pergunta_n3}}
+                </button>
+              </div>
+            </div>
+            <div>
+              <button 
+                type="button" 
+                class="btn-lg btn-primary" 
+                @click="()=> {indice -= 1; status = false}"
+              >
+                Questão Anterior
+              </button>
+              <button 
+                type="button" 
+                class="btn-lg btn-primary" 
+                @click="finalizarQuiz()"
+              >
+                Finalizar
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <div id="botoes" class="d-flex flex-column">
-      <div class="d-flex align-items-center justify-content-around mt-3">
-        <div id="resposta">
-          <button 
-            type="button" 
-            class="btn-lg btn-primary"
-            @click="compararResposta(1)"
-          >
-            {{this.items.resposta}}
-          </button> 
-        </div>
-        <div id="pergunta_um">
-          <button 
-            type="button" 
-            class="btn-lg btn-success"
-            @click="compararResposta(2)"
-          >
-            {{this.items.pergunta_n1}}
-          </button>
-        </div>
-      </div>
-      <div class="d-flex align-items-center justify-content-around mt-3">
-        <div id="pergunta_dois">
-          <button 
-            type="button" 
-            class="btn-lg btn-danger"
-            @click="compararResposta(3)"
-          >
-            {{this.items.pergunta_n2}}
-          </button>
-        </div>
-        <div id="pergunta_tres">
-          <button 
-            type="button" 
-            class="btn-lg btn-warning"
-            @click="compararResposta(4)"
-            >
-             {{this.items.pergunta_n3}}
-          </button>
-        </div>
-      </div>
-      <div>
-        <button 
-          type="button" 
-          class="btn-lg btn-primary" 
-          @click="finalizarQuiz(items.id)"
-        >
-          Finalizar
-        </button>
-      </div>
+    <div v-else-if="finalizado === true">
+      <ScorePortugues :pontuacao="this.escolha" />
     </div>
   </div>
 </template>
 
 <script>
-import axios from "axios";
-import { useRouter, useRoute } from 'vue-router'
+import axios                    from "axios";
+import { useRoute }  from 'vue-router'
+import ScorePortugues           from "@/views/Score/ScorePortugues.vue" 
 
 export default {
-  name: "ManageQuizView",
+  name: "QuestionsPortugues",
+
+  components: {
+    ScorePortugues
+  },
 
   data(){
-    const route = useRouter()
     const routes = useRoute()
+
+    let indice = 0
+    let finalizado = false
+    let status = false
 
     return{
       item: [],
@@ -93,9 +238,11 @@ export default {
         numero_quatro: 4,
       },
 
-      route,
+      indice,
+      finalizado,
+      status,
       routes,
-      escolha: "",
+      escolha: 0,
     }
   },
 
@@ -114,65 +261,58 @@ export default {
     },
 
     // Receber os valores referentes aos quizes
-    async receberQuiz(id) {
-        try {
-          const data = await axios.patch(`${`http://localhost:3000/portugues`}/${id}`,{});
-          this.items = data.data
-        }catch (error) {
-          console.error(error);
-        }
-    },
-
-    compararResposta(id) {
-      if(id === 1){
-        this.escolha = this.items.resposta
-
-        this.items.pontuacao_recebida = this.items.pontuacao
-        
-        this.salvarQuiz()
-
-        this.finalizarQuiz(this.items.id)
-      } else if (id === 2){
-        this.escolha = this.items.pergunta_n1
-
-        this.items.pontuacao_recebida = 0
-        
-        this.salvarQuiz()
-
-        this.finalizarQuiz(this.items.id)
-      } else if (id === 3){
-        this.escolha = this.items.pergunta_n2
-
-        this.items.pontuacao_recebida = 0
-        
-        this.salvarQuiz()
-
-        this.finalizarQuiz(this.items.id)
-      } else {
-        this.escolha = this.items.pergunta_n3
-
-        this.items.pontuacao_recebida = 0
-        
-        this.salvarQuiz()
-
-        this.finalizarQuiz(this.items.id)
-      }
-    },
-    
-    // Finaliza o quiz e direciona para a tela do score
-    finalizarQuiz(id) {
-      this.route.push({name: 'ScorePortugues', params: {id} })
-    },
-
-        // Salvar
-    async salvarQuiz() {
+    async receberQuiz() {
       try {
-        const data = await axios.put(`${`http://localhost:3000/portugues`}/${this.routes.params.id}`, this.items)
-        return data
+        const res = await axios.get(`http://localhost:3000/portugues`);
+        this.items = res.data;
+
       } catch (error) {
-        console.error(error);
+        console.log(error);
       }
     },
+
+    compararResposta(id, pontuacao) {
+      if(id === 1){
+        if(this.status === false) {
+          this.escolha += pontuacao
+
+          this.status = true
+        }
+
+        console.log(this.escolha)
+        console.log(this.status)
+      } else if (id === 2){
+        if(this.status === false && pontuacao - this.escolha <= 0) {
+          this.escolha -= pontuacao
+
+          this.status = true
+        }
+
+        console.log(this.escolha)
+
+      } else if (id === 3){
+        if(this.status === false && pontuacao - this.escolha <= 0) {
+          this.escolha -= pontuacao
+
+          this.status = true
+        }
+        console.log(this.escolha)
+
+      } else {
+        if(this.status === false && pontuacao - this.escolha <= 0) {
+          this.escolha -= pontuacao
+
+          this.status = true
+        }
+        console.log(this.escolha)
+
+      }
+    },
+
+    finalizarQuiz(){
+      this.finalizado = true
+      this.indice = 10
+    }
   }
 };
 </script>
