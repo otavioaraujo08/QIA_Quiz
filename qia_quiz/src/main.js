a4643f8c7c53aab430606fa42ea01c5871ffdcbb
Otávio Araujo <51797012+otavioaraujo08@users.noreply.github.com>
2022-05-17 09:10:59 -0300

    Instalando e configurando tailwind css

--- qia_quiz/src/main.js
+++ qia_quiz/src/main.js
@@ -5,5 +5,6 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
+import './global.css'
 
 createApp(App).use(router).mount('#app')
---
+++ qia_quiz/src/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  purge: ["./index.html", "./src/**/*.{vue, js, ts, jsx, tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
