13661594cf3aa4541fc4f9b8f4e8434c523c5bcb
Otávio Araujo <51797012+otavioaraujo08@users.noreply.github.com>
2022-05-18 01:08:06 -0300

    Reformulando Página de Login

--- qia_quiz/src/views/Home/Home.vue
+++ qia_quiz/src/views/Home/Home.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="home">
-    <logo />
-    <router-link to="/Login">
-      <button type="button" class="btn btn-lg btn-success">Entrar</button>
-    </router-link>
+    <div class="vidro">
+      <logo />
+      <br>
+      <router-link to="/Login">
+        <a class="btn">Entrar</a>
+      </router-link>
+    </div>
   </div>
 </template>
 
@@ -18,13 +21,52 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .home{
+  background: url("../../../public/circuit.svg");
   display: flex;
   align-items: center;
   justify-content: center;
   flex-direction: column;
   margin: 0 auto;
   height: 100vh;
+}
+
+.vidro {
+  width: 70vh;
+  height: 55vh;
+  background: rgba( 255, 255, 255, 0.3 );
+  box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+  backdrop-filter: blur( 5px );
+  -webkit-backdrop-filter: blur( 5px );
+  border-radius: 10px;
+  border: 1px solid rgba( 255, 255, 255, 0.18 );
+}
+
+.btn {
+  background: #4a8dba;
+  background-image: -webkit-linear-gradient(top, #4a8dba, #179aeb);
+  background-image: -moz-linear-gradient(top, #4a8dba, #179aeb);
+  background-image: -ms-linear-gradient(top, #4a8dba, #179aeb);
+  background-image: -o-linear-gradient(top, #4a8dba, #179aeb);
+  background-image: linear-gradient(to bottom, #4a8dba, #179aeb);
+  -webkit-border-radius: 28;
+  -moz-border-radius: 28;
+  border-radius: 28px;
+  font-family: Arial;
+  color: #ffffff;
+  font-size: 20px;
+  padding: 10px 20px 10px 20px;
+  text-decoration: none;
+}
+
+.btn:hover {
+  background: #3cb0fd;
+  background-image: -webkit-linear-gradient(top, #3cb0fd, #3498db);
+  background-image: -moz-linear-gradient(top, #3cb0fd, #3498db);
+  background-image: -ms-linear-gradient(top, #3cb0fd, #3498db);
+  background-image: -o-linear-gradient(top, #3cb0fd, #3498db);
+  background-image: linear-gradient(to bottom, #3cb0fd, #3498db);
+  text-decoration: none;
 }
 </style>
